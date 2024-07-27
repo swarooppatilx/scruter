e59f6ca3f934fea963f06c861f1920b376789f83
swarooppatilx <swarooppatil850@gmail.com>
2024-07-27 18:19:57 +0530

    PWA Fixed

---
+++ public/sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'buzzlist-v1';
+const urlsToCache = [
+  '/icon.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
