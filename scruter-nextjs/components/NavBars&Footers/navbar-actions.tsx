dee513b1d33f7c59456a9de0436ba0e1413ba07e
Shivansh Saxena <112249407+ShivanshPlays@users.noreply.github.com>
2024-10-31 16:38:57 +0530

    Seller login (#348)
    * initialised next app and added homepage
    
    * setup email, prisma, DB schema, nextAUTH
    
    * fic
    
    * seller signup
    
    * added backend and frontend for seller login

--- scruter-nextjs/components/NavBars&Footers/navbar-actions.tsx
+++ scruter-nextjs/components/NavBars&Footers/navbar-actions.tsx
@@ -21,15 +21,12 @@ const NavbarActions = () => {
     return ( 
         <div className="ml-auto flex items-center gap-x-4">
             <Button
-                onClick={()=>router.push("/auth/seller/signup")}
+                onClick={()=>router.push("/auth/seller/login")}
                 className="flex items-center rounded-full bg-black px-4 py-2">
                 <LogIn
                     size={20}
                     color="white"
-                />  
-                <span className="ml-2 text-sm font-medium text-white">
-                    Signup
-                </span>
+                />
             </Button>
         </div>
      );
