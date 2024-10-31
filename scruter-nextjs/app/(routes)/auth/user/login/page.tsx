39ca948604cfaa36b843fb1e6e9a9dcfc0a1d6dc
Shivansh Saxena <112249407+ShivanshPlays@users.noreply.github.com>
2024-10-31 19:32:48 +0530

    global session management throughout the website,  User Login functionality with secure OTP-based email verification,   includes an .env.example (#375)
    * initialised next app and added homepage
    
    * setup email, prisma, DB schema, nextAUTH
    
    * fic
    
    * conflict
    
    * user signup frontend and backend
    
    * auth functionality complete
    
    * added .env.example

--- scruter-nextjs/app/(routes)/auth/user/login/page.tsx
+++ scruter-nextjs/app/(routes)/auth/user/login/page.tsx
@@ -4,7 +4,8 @@ import Link from 'next/link';
 
 import { cn } from '@/lib/utils';
 import { buttonVariants } from '@/components/ui/button';
-import { UserSignupForm } from '../../components/user/signup-form';
+import { SellerLoginForm } from '../../components/seller/login-form';
+import { UserLoginForm } from '../../components/user/login-form';
 
 export const metadata: Metadata = {
   title: 'Authentication',
@@ -14,7 +15,7 @@ export const metadata: Metadata = {
 export default function AuthenticationPage() {
   return (
     <>
-      <div className="md:hidden">
+      <div className="sm:hidden">
         <Image
           src="/userAuth1.svg"
           width={1280}
@@ -31,56 +32,31 @@ export default function AuthenticationPage() {
         />
       </div>
       <div className="container relative hidden h-[700px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-      <Link
-          href="/auth/seller/signup"
-          className={cn(
-            buttonVariants({ variant: 'ghost' }),
-            'absolute right-32 top-8 md:right-32 md:top-8 bg-black text-white'
-          )}
-        >
-          Join Scruter as a seller
-        </Link>
         <Link
-          href="/auth/user/login"
+          href="/auth/user/signup"
           className={cn(
             buttonVariants({ variant: 'ghost' }),
-            'absolute right-4 top-4 md:right-8 md:top-8 bg-black text-white'
+            'absolute right-4 top-4 md:right-8 md:top-8  bg-black text-white'
           )}
         >
-          Login
+          Signup
         </Link>
        
         <div className={`hidden mt-20 bg-[url("/userAuth1.svg")] bg-opacity-50 h-full flex-col bg-no-repeat bg-contain text-white dark:border-r lg:flex`}>
-         
+        
         </div>
         <div className="lg:p-8">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
               <h1 className="text-2xl font-semibold tracking-tight">
-                Create an account
+                Welcome back dear User!
               </h1>
               <p className="text-sm text-muted-foreground">
-                Enter your email below to create your account
+                Enter your email below to Log into your account
               </p>
             </div>
-            <UserSignupForm authType='signup'/>
-            <p className="px-8 text-center text-sm text-muted-foreground">
-              By clicking continue, you agree to our{' '}
-              <Link
-                href="/terms"
-                className="underline underline-offset-4 hover:text-primary"
-              >
-                Terms of Service
-              </Link>{' '}
-              and{' '}
-              <Link
-                href="/privacy"
-                className="underline underline-offset-4 hover:text-primary"
-              >
-                Privacy Policy
-              </Link>
-              .
-            </p>
+            <UserLoginForm authType='login'/>
+            
           </div>
         </div>
       </div>
