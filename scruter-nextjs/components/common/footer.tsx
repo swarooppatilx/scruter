719b861a97342b4b9cd8016c001a748f71d6ae1a
Mehul <mehulprajapati2802@gmail.com>
2024-11-04 15:18:04 -0500

    added support page

--- scruter-nextjs/components/common/footer.tsx
+++ scruter-nextjs/components/common/footer.tsx
@@ -22,7 +22,7 @@ const Helpdesk = [
   { name: 'HelpCenter', id: 1, href: '/help' },
   { name: 'FAQ', id: 2, href: '/faq' },
   { name: 'ContactUs', id: 3, href: '/contactus' },
-  { name: 'Support', id: 4, href: '/Support' },
+  { name: 'Support', id: 4, href: '/support' },
 ];
 
 const Footer = () => {
