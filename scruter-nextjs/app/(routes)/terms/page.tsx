b5830433c3cdfd34c8ecdad0bac886af28dee18f
Abhi-hertz <93651229+AE-Hertz@users.noreply.github.com>
2024-11-06 14:53:40 +0530

    feat: resp and direct links

--- scruter-nextjs/app/(routes)/terms/page.tsx
+++ scruter-nextjs/app/(routes)/terms/page.tsx
@@ -11,7 +11,8 @@ const TermsPage = () => {
   useEffect(() => {
     const mainContent = mainContentRef.current;
 
-    if (mainContent) {
+    // custom scroll behavior for large screens
+    if (mainContent && window.innerWidth >= 1024) {
       const handleScroll = (event: WheelEvent) => {
         event.preventDefault();
         const newScrollTop = Math.max(0, mainContent.scrollTop + event.deltaY);
@@ -24,32 +25,34 @@ const TermsPage = () => {
   }, []);
 
   return (
-    <div className="min-h-screen bg-gray-900 text-gray-300 flex flex-col w-full overflow-hidden selection:bg-cyan-300 selection:text-cyan-900 ">
+    <div className="min-h-screen bg-gray-900 text-gray-300 flex flex-col w-full overflow-hidden selection:bg-cyan-300 selection:text-cyan-900">
       {/* Header Section */}
-      <header className="bg-custom-gradient dark:bg-dark-custom-gradient pt-28 pb-28 pl-10 text-start w-full">
-        <h1 className="text-4xl lg:text-7xl w-4/5 font-bold mb-2 bg-clip-text text-transparent bg-text-gradient">Terms of Service</h1>
-        <p className="text-gray-200 text-lg">Published and effective on: April 30, {new Date().getFullYear()}</p>
+      <header className="bg-custom-gradient dark:bg-dark-custom-gradient pt-20 pb-10 px-4 lg:pt-28 lg:pb-28 lg:px-10 text-start w-full">
+        <h1 className="text-3xl md:text-4xl lg:text-7xl font-bold mb-2 bg-clip-text text-transparent bg-text-gradient">
+          Terms of Service
+        </h1>
+        <p className="text-gray-200 text-sm md:text-lg">Published and effective on: April 30, {new Date().getFullYear()}</p>
       </header>
 
-      <div className="flex w-full">
+      <div className="flex flex-col lg:flex-row w-full h-full">
         {/* Main Content */}
         <main
           ref={mainContentRef}
-          className="w-4/5 p-8 bg-white dark:bg-black shadow-lg h-[calc(100vh-160px)] overflow-y-scroll scroll-smooth custom-scrollbar"
+          className="w-full lg:w-4/5 p-4 lg:p-8 bg-white dark:bg-black shadow-lg h-auto lg:h-[calc(100vh-160px)] overflow-y-auto lg:overflow-y-scroll scroll-smooth custom-scrollbar"
         >
           {TERMS_CONTENT.map(({ id, icon, title, content }) => (
             <section id={id} key={id} className="mb-8">
-              <h3 className="text-2xl text-black dark:text-white flex items-center mb-2">
+              <h3 className="text-xl md:text-2xl text-black dark:text-white flex items-center mb-2">
                 <FontAwesomeIcon icon={icon} className="mr-2 text-[#ff476b]" />
                 {title}
               </h3>
-              <p className="text-gray-500 text-justify">{content}</p>
+              <p className="text-gray-500 text-justify text-sm md:text-base">{content}</p>
             </section>
           ))}
         </main>
 
         {/* Right Sidebar */}
-        <aside className="w-1/5 p-6 bg-white dark:bg-black text-gray-400 sticky top-0 h-[calc(100vh-160px)]">
+        <aside className="hidden lg:block w-1/5 p-6 bg-white dark:bg-black text-gray-400 sticky top-0 h-[calc(100vh-160px)]">
           <h2 className="text-gray-600 text-xs uppercase font-semibold mb-4">On This Page</h2>
           <ul>
             {TERMS_CONTENT.map(({ id, title }) => (
@@ -63,7 +66,7 @@ const TermsPage = () => {
                       }
                     }
                   }}
-                  className="text-sm hover:text-black dark:text-white transition-colors duration-200"
+                  className="text-sm hover:text-black dark:hover:text-white transition-colors duration-200"
                 >
                   {title}
                 </button>
@@ -73,15 +76,15 @@ const TermsPage = () => {
 
           {/* Contribute Section */}
           <div className="mt-8">
-            <h2 className="text-gray-400 text-xs uppercase font-semibold mb-2">Contribute</h2>
+            <h2 className="text-gray-600 text-xs uppercase font-semibold mb-2">Contribute</h2>
             <ul>
               <li>
-                <a href="#report-issue" className="text-sm hover:text-white transition-colors duration-200">
+                <a href="https://github.com/swarooppatilx/scruter/issues/new?assignees=&labels=bug&projects=&template=bug.yml&title=%5BBUG%5D+" target='_blank' className="text-sm hover:text-black dark:hover:text-white transition-colors duration-200">
                   Report an issue
                 </a>
               </li>
               <li>
-                <a href="#request-feature" className="text-sm hover:text-white transition-colors duration-200">
+                <a href="https://github.com/swarooppatilx/scruter/issues/new?assignees=&labels=enhancement&projects=&template=feature.yml&title=%5BFeat%5D" target='_blank' className="text-sm hover:text-black dark:hover:text-white transition-colors duration-200">
                   Request a feature
                 </a>
               </li>
