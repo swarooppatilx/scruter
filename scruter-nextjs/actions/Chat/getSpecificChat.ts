63c5515257e9d537047ccff01dbd65afd3145d08
Shivansh <shivanshsaxena2910@gmail.com>
2024-11-09 23:37:23 +0530

    added schema and server actions of chat

---
+++ scruter-nextjs/actions/Chat/getSpecificChat.ts
@@ -0,0 +1,22 @@
+import prismadb from "@/lib/prismadb";
+import { ChatResponse } from "./createChat";
+
+export async function getSpecificChat(
+    userId: string,
+    sellerId: string,
+    listingId: string
+  ): Promise<ChatResponse> {
+    try {
+      const chat = await prismadb.chat.findFirst({
+        where: { userId, sellerId, listingId },
+        include: { messages: true },
+      });
+      if (!chat) {
+        return { success: false, error: "Chat not found" };
+      }
+      return { success: true, data: chat };
+    } catch (error) {
+      console.error("Error fetching chat:", error);
+      return { success: false, error: "Failed to fetch chat" };
+    }
+  }
